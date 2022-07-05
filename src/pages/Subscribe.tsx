aa6255e985d3dcddb14a36510144306493bb0797
felipeleite11 <lipi4shared@hotmail.com>
2022-07-05 01:44:35 -0300

    Corrigindo importação da imagem da tela de inscrição.

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,7 +2,10 @@ import { useState, FormEvent } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { Logo } from '../components/Logo'
+
 import { useCreateSubscriberMutation } from '../graphql/generated'
+
+const mockup = '/src/assets/code-mockup.png'
 
 export function Subscribe() {
 	const navigate = useNavigate()
@@ -72,7 +75,7 @@ export function Subscribe() {
 				</div>
 			</div>
 
-			<img src="/src/assets/code-mockup.png" alt="" className="mt-10" />
+			<img src={mockup} alt="" className="mt-10" />
 		</div>
 	)
 }
