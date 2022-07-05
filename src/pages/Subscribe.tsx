2bd9d7a23b2601db9f6e71f2b49f0c74052c7b02
felipeleite11 <lipi4shared@hotmail.com>
2022-07-05 01:36:50 -0300

    Corrigindo importação.

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,7 +1,7 @@
 import { useState, FormEvent } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { Logo } from '../components/logo'
+import { Logo } from '../components/Logo'
 import { useCreateSubscriberMutation } from '../graphql/generated'
 
 export function Subscribe() {
